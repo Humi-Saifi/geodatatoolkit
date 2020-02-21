b65c16ba8c54bcefcfe32119e4773d8ce9182299
humaira <homairasaifi@gmail.com>
2020-02-21 22:27:54 +0100

    Completed SYLVAC & DISTO

--- geodatatoolkit/views.py
+++ geodatatoolkit/views.py
@@ -28,7 +28,7 @@ def ApiSensor1(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM disto")
         all_data=dictfetchall(cursor)
-    print(all_data)
+    #print(all_data)
     #return render(request,"sensors.html",{})
     return JsonResponse(all_data,safe=False)
 
@@ -37,7 +37,7 @@ def ApiSensor2(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM gpsnn")
         all_data=dictfetchall(cursor)
-    print(all_data)
+    #print(all_data)
     #return render(request,"sensors.html",{})
     return JsonResponse(all_data,safe=False)  
 
@@ -46,7 +46,7 @@ def ApiSensor3(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM sensordata")
         all_data=dictfetchall(cursor)
-    print(all_data)
+    #print(all_data)
     #return render(request,"sensors.html",{})
     return JsonResponse(all_data,safe=False)     
 
@@ -120,13 +120,19 @@ def runSYLVAC(request,sensor,com_port,baudrate,bytesize,parity):
                     zeit = hh + mm + str(ss)
                 name = 'D:/Neuer Ordner (2)/data/PPython' + 'sylvac' + datum + zeit + '.txt'
                 filename = name
+                sylvac_status_file="sylvac_status.txt"
                 print(name)
                 datei = open(name, "w")
-
+                sylvac_status=open(sylvac_status_file,"w")
+                sylvac_status.write('1')
+                sylvac_status.close()
                 print(n)
                 print(a)
-
-                while globals.request.COOKIES['sylvac']:
+                #infinite loop
+                sylvac_status_read=open(sylvac_status_file,"r")
+                status=int(sylvac_status_read.read())
+                sylvac_status_read.close()
+                while status==1:
                     print('COOKIES')
                     print(request.COOKIES['sylvac'])
                     print(globals.request.COOKIES['sylvac'])
@@ -152,6 +158,9 @@ def runSYLVAC(request,sensor,com_port,baudrate,bytesize,parity):
                     conn_local.commit()
                     conn_remote.commit()
                     print("record inserted in sensordata table.")
+                    sylvac_status_read=open(sylvac_status_file,"r")
+                    status=int(sylvac_status_read.read())
+                    sylvac_status_read.close()
                 print("SYLVAC COMPLETED TASK")
             datei.close()
     
@@ -159,37 +168,135 @@ def runSYLVAC(request,sensor,com_port,baudrate,bytesize,parity):
     cursor_remote.close()
     conn_local.close()
     conn_remote.close()
+    return JsonResponse({"data": "sylvac task complete"},safe=False)
 
 
 def stopSYLVAC(request):
-    request.COOKIES["sylvac"]=False
-    globals.request.COOKIES["sylvac"]=False
+    sylvac_status_file="sylvac_status.txt"
+    sylvac_status=open(sylvac_status_file,"w")
+    sylvac_status.write('0')
+    sylvac_status.close()
     print("STOPPING SYLVAC")
     return JsonResponse({"data": True},safe=False)
 
 
 def runDISTO(requet,sensor,com_port,baudrate,bytesize,parity):
-    pass
+    port=com_port
+    conn_local=psycopg2.connect(user="postgres",password="postgres",host="localhost",port="5432",database="sensors")
+    conn_remote=psycopg2.connect(user="dssbcuwcqanxot",password="47e67cdfdb55aed7e79847bf4b7811c640c5835a49f80e102ea9434b7669e95e",host="ec2-54-246-89-234.eu-west-1.compute.amazonaws.com",port="5432",database="d4sboi6172opf9")
+    cursor_local=conn_local.cursor()
+    cursor_remote=conn_remote.cursor()
+    ser = serial.Serial(port=com_port,
+                        baudrate=int(baudrate),
+                        bytesize=getByteSize(int(bytesize)),
+                        parity=getParity(int(parity)),
+                        timeout=200)
+    if sensor=="DISTO":
+        if ser.isOpen() == True:
+
+            filename = ''
+            a = 0
+            i = 0
+
+            n = 5
+            nn = 1
+
+            while a < nn:
+                a = 0
+                d = str(datetime.today())
+                z = d.split(" ")
+                da = z[0]
+                date = da.split("-")
+                jahr = date[0]
+                monat = date[1]
+                tag = date[2]
+                datum = jahr + monat + tag
+                ti = z[1]
+                time_ = ti.split(":")
+                hh = time_[0]
+                mm = time_[1]
+                ss = time_[2]
+                ss = int(float(ss))
+                if ss < 10:
+                    ss = '0' + str(ss)
+                    zeit = hh + mm + ss
+                else:
+                    zeit = hh + mm + str(ss)
+                name = 'D:/Neuer Ordner (2)/data/PPython' + 'disto' + datum + zeit + '.txt'
+                filename = name
+                print(name)
+                datei = open(name, "w")
+                #print(n)
+                #print(a)
+                disto_status_file="disto_status.txt"
+                disto_status=open(disto_status_file,"w")
+                disto_status.write('1')
+                disto_status.close()
+                status=1    
+                while status==1:
+                    ser.write(b"g\r\n")
+                    line = ser.readline(40)  # lese eine Zeile aus dem seriellen Port
+                    zeile = str(line)
+                    t = time.time()
+                    print('%10.3f' % t)
+                    datei.write('%10.3f' % t + ' ' + zeile[9:15] + '\n')  # schreiben in eine Datei
+                    time.sleep(2)
+                    a += 1
+                    #sql = "INSERT INTO disto (date, value) VALUES (%s, %s)"
+                    print("-------DISTO VALUE------")
+                    print(zeile[9:17])
+                    sql2 ="INSERT INTO disto(value) VALUES(%s)" %(zeile[9:17])
+                    #val = ('%10.3f'% t, zeile[9:17])
+                    #cur.execute(sql, val)
+                    #conn.commit()
+                    cursor_local.execute(sql2)        
+                    cursor_remote.execute(sql2)
+                    conn_local.commit()
+                    conn_remote.commit()
+
+                    print("record inserted in disto table.")
+                    disto_status_read=open(disto_status_file,"r")
+                    status=int(disto_status_read.read())
+                    disto_status_read.close()
+                print('STOPPING DISTO')
+                datei.close()
+
+        cursor_local.close()
+        cursor_remote.close()
+        conn_local.close()
+        conn_remote.close()
+        return JsonResponse({"data":"disto task completed"},safe=False)      
 
 def stopDISTO(request):
-    request.session["disto"]=False
+    disto_status_file="disto_status.txt"
+    disto_status=open(disto_status_file,"w")
+    disto_status.write('0')
+    disto_status.close()
+    print("STOPPING DISTO")
     return JsonResponse({"data": True},safe=False)
 
 def runGPSNN(request,sensor,com_port,baudrate,bytesize,parity):
     pass
 
 def stopGPSNN(request):
-    request.session["gpsnn"]=False
+    gpsnn_status_file="gpsnn_status.txt"
+    gpsnn_status=open(gpsnn_status_file,"w")
+    gpsnn_status.write('0')
+    gpsnn_status.close()
+    print("STOPPING GPSNN")
     return JsonResponse({"data": True},safe=False)
 
 
 def deconnect(request):
     sensor=request.POST['sensor']
     if sensor=='SYLVAC':
+        print('STOP SYLVAC')
         stopSYLVAC(request)
     elif sensor=='DISTO':
+        print('STOP DISTO')
         stopDISTO(request)
     elif sensor=='GPSNN':
+        print('STOP GPSNN')
         stopGPSNN(request)
 
 async def AsyncResponse(request,sensor):
@@ -205,11 +312,14 @@ def runComPort(request):
     bytesize=request.POST['bytesize']
     port=com_port
     if sensor=='SYLVAC':
+        print('running SYLVAC')
         runSYLVAC(request,sensor,com_port,baudrate,bytesize,parity)
     elif sensor=='DISTO':
-        pass
+        print('running DISTO')
+        runDISTO(request,sensor,com_port,baudrate,bytesize,parity)
     elif sensor=='GPSNN':
-        pass
+        print('running GPSNN')
+        runGPSNN(request,sensor,com_port,baudrate,bytesize,parity)
 
     
     
